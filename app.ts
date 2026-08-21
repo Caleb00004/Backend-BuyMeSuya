ebef35b15e21cc62fc5ced10d5a05930dd49f019
Caleb00004 <calebakpan7@gmail.com>
2026-08-21 22:13:58 +0100

    cors fix for production

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ console.log("Mongo URI loaded:", process.env.MONGO_URI ? "yes" : "MISSING");
 // Allow localhost in dev, only your frontend URL in prod
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? ["https://buy-me-suya.vercel.app",] // ✅ production domain
+    ? [process.env.APP_BASE_URL] // ✅ production domain
     : ["http://localhost:3000", "http://localhost:3001"]; // ✅ dev frontend
 
 export const app = express();
