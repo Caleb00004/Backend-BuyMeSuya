ca1404ca3f0f3a08fe3f7aee00ce2437eb76a959
Caleb00004 <calebakpan7@gmail.com>
2026-09-02 23:50:10 +0100

    added console logs for auth middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -7,6 +7,7 @@ export const auth = (req: AuthRequest, res: Response, next: NextFunction) => {
     const token = req.cookies?.accessToken;
 
     if (!token) {
+        console.error("[auth] No accessToken cookie present. Path:", req.originalUrl);
         return res.sendStatus(401);
     }
 
@@ -18,6 +19,7 @@ export const auth = (req: AuthRequest, res: Response, next: NextFunction) => {
 
         next();
     } catch (err) {
+        console.error("[auth] Error verifying access token:", err);
         return res.sendStatus(401);
     }
 };
